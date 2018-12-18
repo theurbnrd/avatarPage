c37b6afbdb854cca56fdaba12d35c98c36ed8e7e
DeJohn Smith <dejohnsmith@icstars.org>
2018-12-18 10:16:35 -0600

    Add files via upload

---
+++ home/home.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Slide } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+}
